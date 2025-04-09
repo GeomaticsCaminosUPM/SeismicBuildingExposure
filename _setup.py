e8cfa0933fa5e8cca2703ccd505a0ff4d1a963d2
Miguel Ureña Pliego <94168783+MiguelUrenaPliego@users.noreply.github.com>
2025-04-09 14:17:21 +0200

    Update and rename SeismicBuildingExposure/footprint/setup.py to _setup.py

--- _setup.py
+++ _setup.py
@@ -8,16 +8,26 @@ setup(
     long_description_content_type="text/markdown",
     author="Miguel Ureña Pliego",
     author_email="miguel.urena@upm.es",
-    url="https://github.com/GeomaticsCaminosUPM/footprint_attributes",
+    url="https://github.com/GeomaticsCaminosUPM/SeismicBuildingExposure",
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "geopandas>=1.0.0",
-        "pandas>=1.3.0",
-        "shapely>=1.8.0",
         "numpy>=1.21.0",
-        "scipy>=1.0.0"
     ],
+    extras_require={
+        "footprint": [
+           "scipy>=1.0.0",
+        ],
+        "height": [
+            "rasterio>=1.0.0",
+            "scipy>=1.0.0",
+        ],
+        "all": [
+            "geopandas>=1.0.0",
+            "pandas>=1.3.0",
+            "shapely>=1.8.0",
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
